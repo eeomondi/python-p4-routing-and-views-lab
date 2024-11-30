259715de424961d0146a589ea420531f4ebf658b
Elvis <elvis.omondi@student.moringaschool.com>
2024-11-30 11:53:44 +0300

    commit

--- server/app.py
+++ server/app.py
@@ -1,8 +1,37 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:text>')
+def print_string(text):
+    print(text)  # Prints to the console
+    return f'<p>{text}</p>'
+
+@app.route('/count/<int:number>')
+def count(number):
+    return '<br>'.join(str(i) for i in range(1, number + 1))
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2 if num2 != 0 else "Error: Division by zero"
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return f"Operation {operation} not supported"
+    
+    return f'{num1} {operation} {num2} = {result}'
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True)
+
